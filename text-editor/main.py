1a6c3ede77e76b9d2b67cf8e34ecd0f3060f12d1
aleksmn <al-mn@yandex.ru>
2022-12-09 13:20:55 +0300

    init

--- text-editor/main.py
+++ text-editor/main.py
@@ -1,48 +1,59 @@
+'''Simple Text Editor'''
 import tkinter as tk
-from tkinter.filedialog import askopenfilename, asksaveasfilename 
- 
- 
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import customtkinter as ctk
+
+# Modes: "System" (standard), "Dark", "Light"
+ctk.set_appearance_mode("dark")
+# Themes: "blue" (standard), "green", "dark-blue"
+ctk.set_default_color_theme("blue")
+
+
 def open_file():
-    """Открываем файл для редактирования"""
+    '''Открываем файл для редактирования'''
     filepath = askopenfilename(
         filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
     )
     if not filepath:
         return
-    txt_edit.delete("1.0", tk.END)
-    with open(filepath, "r") as input_file:
+    txt_edit.delete("0.0", tk.END)
+
+    with open(filepath, "r", encoding='utf-8') as input_file:
         text = input_file.read()
         txt_edit.insert(tk.END, text)
-    window.title(f"Простой текстовый редактор - {filepath}")
- 
+    root.title(f"Простой текстовый редактор - {filepath}")
+
+
 def save_file():
-    """Сохраняем текущий файл как новый файл."""
+    '''Сохраняем текущий файл как новый файл.'''
     filepath = asksaveasfilename(
-        defaultextension="txt",
+        defaultextension=".txt",
         filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")],
     )
     if not filepath:
         return
-    with open(filepath, "w") as output_file:
-        text = txt_edit.get("1.0", tk.END)
+    with open(filepath, "w", encoding='utf-8') as output_file:
+        text = txt_edit.get("0.0", tk.END)
         output_file.write(text)
-    window.title(f"Простой текстовый редактор - {filepath}")
- 
- 
-window = tk.Tk()
-window.title("Простой текстовый редактор")
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
- 
-txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(fr_buttons, text="Открыть", command=open_file)
-btn_save = tk.Button(fr_buttons, text="Сохранить как...", command=save_file)
- 
+    root.title(f"Простой текстовый редактор - {filepath}")
+
+
+root = ctk.CTk()
+root.title("Простой текстовый редактор")
+root.rowconfigure(0, minsize=500, weight=1)
+root.columnconfigure(1, minsize=500, weight=1)
+
+txt_edit = ctk.CTkTextbox(root)
+
+fr_buttons = ctk.CTkFrame(root)
+btn_open = ctk.CTkButton(fr_buttons, text="Открыть", command=open_file)
+btn_save = ctk.CTkButton(
+    fr_buttons, text="Сохранить как...", command=save_file)
+
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
- 
+
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
- 
-window.mainloop()
+
+root.mainloop()

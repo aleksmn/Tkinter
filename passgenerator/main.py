b0bffcccd5593d5d774313eb3b27fcbd4496d78c
aleksmn <al-mn@yandex.ru>
2023-01-19 17:56:32 +0300

    init

--- passgenerator/main.py
+++ passgenerator/main.py
@@ -4,12 +4,45 @@ from generator import generate_password
 
 print(generate_password())
 
+# Functions
+def display_password():
+    """Выводим пароль на экран"""
+    text_output.delete(0, 'end')
+    text_output.insert(0, generate_password())
+
+
+
 
 root = ctk.CTk()
 
-root.geometry('400x200')
+# root.geometry('400x200')
 
-root.title("Password Generator")
+root.title("Pass Generator")
+root.resizable(0, 0)
 
+
+
+text_output = ctk.CTkEntry(root,
+                           placeholder_text='PASSWORD',
+                           corner_radius=0,
+                           width=200,
+                           height=40,
+                           justify='center',
+                           font=('monospace', 20, 'bold'))
+
+
+text_output.pack(padx=40, pady=40)
+
+pass_btn = ctk.CTkButton(root, 
+                         text="Generate",
+                         fg_color='slateblue',
+                         hover_color='purple',
+                         corner_radius=0,
+                         font=('sans-serif', 20, 'bold'),
+                         width=200,
+                         height=40,
+                         command=display_password
+                        )
+pass_btn.pack(pady=(0, 40))
 
 root.mainloop()

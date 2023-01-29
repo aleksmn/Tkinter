ef43b0472937c7b55c58066ee7b2eabbf982cc00
aleksmn <al-mn@yandex.ru>
2023-01-29 18:56:43 +0300

    fix

--- passgenerator/main.py
+++ passgenerator/main.py
@@ -2,30 +2,28 @@ import customtkinter as ctk
 from generator import generate_password
 
 
-print(generate_password())
+number_of_symbols = 14
+
 
 # Functions
-def display_password():
+def display_password(number_of_symbols):
     """Выводим пароль на экран"""
     text_output.delete(0, 'end')
-    text_output.insert(0, generate_password())
-
-
+    text_output.insert(0, generate_password(number_of_symbols))
 
 
 root = ctk.CTk()
 
-# root.geometry('400x200')
 
 root.title("Pass Generator")
-root.resizable(0, 0)
 
+root.resizable(0, 0)
 
 
 text_output = ctk.CTkEntry(root,
                            placeholder_text='PASSWORD',
                            corner_radius=0,
-                           width=200,
+                           width=number_of_symbols*14,
                            height=40,
                            justify='center',
                            font=('monospace', 20, 'bold'))
@@ -33,7 +31,7 @@ text_output = ctk.CTkEntry(root,
 
 text_output.pack(padx=40, pady=40)
 
-pass_btn = ctk.CTkButton(root, 
+pass_btn = ctk.CTkButton(root,
                          text="Generate",
                          fg_color='slateblue',
                          hover_color='purple',
@@ -41,8 +39,8 @@ pass_btn = ctk.CTkButton(root,
                          font=('sans-serif', 20, 'bold'),
                          width=200,
                          height=40,
-                         command=display_password
-                        )
-pass_btn.pack(pady=(0, 40))
+                         command=lambda: display_password(number_of_symbols)
+                         )
+pass_btn.pack(padx=40, pady=(0, 40))
 
 root.mainloop()

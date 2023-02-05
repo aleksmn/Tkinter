3484b3ca70cc9fd4cf216f8fdd67dbb945998a29
aleksmn <al-mn@yandex.ru>
2023-02-05 15:39:53 +0300

    fix

--- passgenerator/main.py
+++ passgenerator/main.py
@@ -2,23 +2,27 @@ import customtkinter as ctk
 from generator import generate_password
 
 
-number_of_symbols = 14
+number_of_symbols = 18
 
 
-# Functions
+# print(generate_password(number_of_symbols))
+
+
+ctk.set_appearance_mode("dark")
+
+
+
+# # Functions
 def display_password(number_of_symbols):
     """Выводим пароль на экран"""
     text_output.delete(0, 'end')
     text_output.insert(0, generate_password(number_of_symbols))
 
-
 root = ctk.CTk()
-
 
 root.title("Pass Generator")
 
 root.resizable(0, 0)
-
 
 text_output = ctk.CTkEntry(root,
                            placeholder_text='PASSWORD',
@@ -29,7 +33,7 @@ text_output = ctk.CTkEntry(root,
                            font=('monospace', 20, 'bold'))
 
 
-text_output.pack(padx=40, pady=40)
+text_output.grid(column=0, row=0, padx=40, pady=40)
 
 pass_btn = ctk.CTkButton(root,
                          text="Generate",
@@ -41,6 +45,7 @@ pass_btn = ctk.CTkButton(root,
                          height=40,
                          command=lambda: display_password(number_of_symbols)
                          )
-pass_btn.pack(padx=40, pady=(0, 40))
+
+pass_btn.grid(column=0, row=1, padx=40, pady=(0, 40), sticky='we')
 
 root.mainloop()

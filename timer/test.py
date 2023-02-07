5882156fb2f8d9f5f912e0ff867bc0e524cd4beb
aleksmn <al-mn@yandex.ru>
2023-02-07 19:22:48 +0300

    init

--- timer/test.py
+++ timer/test.py
@@ -1,9 +1,36 @@
-# Дана строка в формате 1:02:15 
+# Дана строка в формате 1:02:15
 # часы или минуты могут отсутсвовать
 # нужно из этой строки извлечь часы, минуты и секунды
 
-string = "12:02:15"
+# string = "12:02:15"
 
 # hours =
-# minutes = 
+# minutes =
 # seconds =
+
+
+time_entry = input("Введите время: ")
+
+hours, minutes, seconds = 0, 0, 0
+
+# Получаем список, например ['12', '02', '15']
+string_split =  time_entry.split(":")
+
+if len(string_split) == 3:
+    hours = int(string_split[0])
+    minutes = int(string_split[1])
+    seconds = int(string_split[2])
+elif len(string_split) == 2:
+    minutes = int(string_split[0])
+    seconds = int(string_split[1])
+elif len(string_split) == 1:
+    seconds = int(string_split[0])
+else:
+    print("Неверный формат ввода")
+
+
+# Проверка
+print('Часы:', hours)
+print('Минуты:', minutes)
+print('Секунды:', seconds)
+

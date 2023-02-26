a019806b1d076c63a5cff88068f7d22678bf87ea
aleksmn <al-mn@yandex.ru>
2023-02-26 11:42:23 +0300

    fix

--- color-theme-maker/main_oop.py
+++ color-theme-maker/main_oop.py
@@ -22,51 +22,46 @@ class ColorFrame(ctk.CTkFrame):
         self.red_value, self.green_value, self.blue_value = '00', '00', '00'
 
         # Define Layout
-        self.color_frame = ctk.CTkFrame(self)
-        self.input_frame = ctk.CTkFrame(self)
 
-        self.color_frame.pack(fill='both', expand=True, pady=(0, 10))
-        self.input_frame.pack(fill='both', expand=True, pady=10)
-
-        self.color_box = ctk.CTkLabel(
-            self.color_frame, fg_color='black', text='', height=100, width=100)
-        self.color_hex = ctk.CTkEntry(self.color_frame)
+        self.color_box = ctk.CTkLabel(self, fg_color='black', text='', height=100, width=100)
+        self.color_hex = ctk.CTkEntry(self, justify='center', width=100)
         self.color_hex.insert('end', "#000000")
 
-        self.color_box.grid(row=0, column=0, padx=35, pady=10)
-        self.color_hex.grid(row=1, column=0)
+        self.color_box.grid(row=0, column=0, columnspan=2, padx=35, pady=10)
+        self.color_hex.grid(row=1, column=0, columnspan=2,)
 
-        self.color_frame.grid_columnconfigure(0, weight=1)
+
 
         # Input frame.
 
         # RED
-        self.red_label = ctk.CTkLabel(self.input_frame, text="R")
-        self.red_slider = ctk.CTkSlider(
-            self.input_frame, from_=0, to=255, orientation='horizontal', command=self.get_red)
-        self.red_label = ctk.CTkLabel(self.input_frame, text="R")
+        self.red_label = ctk.CTkLabel(self, text="R")
+        self.red_slider = ctk.CTkSlider(self, from_=0, to=255, orientation='horizontal', command=self.get_red)
+        self.red_label = ctk.CTkLabel(self, text="R")
         self.red_slider.set(0)
 
         # GREEN
-        self.green_label = ctk.CTkLabel(self.input_frame, text="G")
+        self.green_label = ctk.CTkLabel(self, text="G")
         self.green_slider = ctk.CTkSlider(
-            self.input_frame, from_=0, to=255, orientation='horizontal', command=self.get_green)
+            self, from_=0, to=255, orientation='horizontal', command=self.get_green)
         self.green_slider.set(0)
 
         # BLUE
-        self.blue_label = ctk.CTkLabel(self.input_frame, text="B")
-        self.blue_slider = ctk.CTkSlider(
-            self.input_frame, from_=0, to=255, orientation='horizontal', command=self.get_blue)
+        self.blue_label = ctk.CTkLabel(self, text="B")
+        self.blue_slider = ctk.CTkSlider(self, from_=0, to=255, orientation='horizontal', command=self.get_blue)
         self.blue_slider.set(0)
 
-        self.red_label.grid(row=0, column=0, sticky='W')
-        self.red_slider.grid(row=0, column=1, sticky='W')
 
-        self.green_label.grid(row=1, column=0, sticky='W')
-        self.green_slider.grid(row=1, column=1, sticky='W')
+        # Размещаем все элементы:
 
-        self.blue_label.grid(row=2, column=0, sticky='W')
-        self.blue_slider.grid(row=2, column=1, sticky='W')
+        self.red_label.grid(row=2, column=0, sticky='W', padx=(5, 0))
+        self.red_slider.grid(row=2, column=1, sticky='W', padx=5)
+
+        self.green_label.grid(row=3, column=0, sticky='W', padx=(5, 0))
+        self.green_slider.grid(row=3, column=1, sticky='W', padx=5)
+
+        self.blue_label.grid(row=4, column=0, sticky='W', padx=(5, 0))
+        self.blue_slider.grid(row=4, column=1, sticky='W', padx=5)
 
     def get_red(self, value):
         """Get current value for red and update color box"""
@@ -111,19 +106,18 @@ class App(ctk.CTk):
 
         except:
             pass
-        self.configure(padx=20, pady=20)
+        self.configure(padx=10, pady=10)
+
+        options = {'padx': 10, 'pady': 10}
 
         self.color_frame = ColorFrame(self)
-        self.color_frame.grid(column=0, row=0)
+        self.color_frame.grid(column=0, row=0, **options)
         self.color2_frame = ColorFrame(self)
-        self.color2_frame.grid(column=1, row=0)
+        self.color2_frame.grid(column=1, row=0, **options)
         self.color3_frame = ColorFrame(self)
-        self.color3_frame.grid(column=2, row=0)
+        self.color3_frame.grid(column=2, row=0, **options)
         self.color4_frame = ColorFrame(self)
-        self.color4_frame.grid(column=3, row=0)
-
-
-
+        self.color4_frame.grid(column=3, row=0, **options)
 
 
 if __name__ == "__main__":

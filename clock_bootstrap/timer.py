e744e99aac7b228ba778b64bf248b1dc0b31a473
aleksmn <al-mn@yandex.ru>
2023-11-25 19:48:08 +0300

    add start method

--- clock_bootstrap/timer.py
+++ clock_bootstrap/timer.py
@@ -14,11 +14,36 @@ class TimerFrame(ttk.Frame):
         self.time_entry = ttk.Entry(self)
         self.time_entry.grid(row=0, column=0, padx=10, pady=10)
 
-        self.start_button = ttk.Button(self, text="Start")
+        self.start_button = ttk.Button(self, text="Start", command=self.start)
         self.start_button.grid(row=0, column=1, padx=(0, 10))
 
         self.stop_button = ttk.Button(self, text="Stop", bootstyle="danger")
         self.stop_button.grid(row=1, column=1, padx=(0, 10), pady=(0, 10))
+
+        self.time_label = ttk.Label(self, text="00:00:00", font=('monospace', 18))
+        self.time_label.grid(column=0, row=1)
+
+
+    def start(self):
+        self.stop_loop = False
+        hours, minutes, seconds = 0, 0, 0
+        time_string = self.time_entry.get().split(":")
+        if len(time_string) == 3:
+            hours = int(time_string[0])
+            minutes = int(time_string[1])
+            seconds = int(time_string[2])
+
+        elif len(time_string) == 2:
+            minutes = int(time_string[0])
+            seconds = int(time_string[1])
+
+        elif len(time_string) == 1:
+            seconds = int(time_string[0])
+            
+        else:
+            print("Неверный формат ввода")
+            return
+
 
 
 
@@ -38,5 +63,5 @@ class App(ttk.Window):
 
 # Запуск программы
 if __name__ == "__main__":
-    app = App(title="My Timer", themename='minty', minsize=(200, 0))
+    app = App(title="My Timer", themename='minty', minsize=(100, 0))
     app.mainloop()

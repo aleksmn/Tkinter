77fd5238ec0710f863dc3d2f2f1b29551fcf4866
aleksmn <al-mn@yandex.ru>
2024-01-30 18:48:55 +0300

    fix

--- passgenerator/main.py
+++ passgenerator/main.py
@@ -2,14 +2,15 @@ import customtkinter as ctk
 
 from generator import generate_password
 
+
 # Настройка кол-ва символов в пароле
 number_of_symbols = 18
 
 
-# # Functions
+# # # Functions
 def display_password(number_of_symbols):
     """Выводим пароль на экран"""
-    text_output.delete(0, 'end')
+    text_output.delete(0, "end")
     text_output.insert(0, generate_password(number_of_symbols))
 
 
@@ -23,37 +24,30 @@ root.title("Pass Generator")
 
 root.resizable(0, 0)
 
-root.configure(fg_color='#2A2F4F')
 
 text_output = ctk.CTkEntry(root,
                            placeholder_text='PASSWORD',
                            corner_radius=0,
-                           fg_color='#F0F0F0',
-                           text_color='#555',
-                           width=14*number_of_symbols,
+                           width=16*number_of_symbols,
                            height=40,
                            justify='center',
                            font=('monospace', 20, 'bold'))
 
-
+# размещаем виджет по сетке
 text_output.grid(column=0, row=0, padx=40, pady=40)
 
 
 pass_btn = ctk.CTkButton(root,
                          text="Generate",
-                         fg_color='#39B5E0',
-                         hover_color='#A31ACB',
+                         fg_color='purple',
+                         hover_color='black',
                          corner_radius=0,
                          font=('sans-serif', 20, 'bold'),
                          height=40,
-                         command=lambda:display_password(number_of_symbols))
+                         command=lambda: display_password(number_of_symbols))
 
 
 pass_btn.grid(column=0, row=1, padx=40, pady=(0, 40), sticky="we")
-
-
-
-
 
 
 

8e653701809a1984281dde4baf0a7bb8f7cc2ab1
aleksmn <al-mn@yandex.ru>
2024-03-02 12:35:15 +0300

    add lables

--- kanban_board/main.py
+++ kanban_board/main.py
@@ -11,8 +11,7 @@ def move_task(event, source_list, target_list=None):
     if selected:
         # Получение выбранной задачи
         task = source_list.get(selected)
-        print(task)
-        # Перемещение задачи из одной доски в другую
+        # # Перемещение задачи из одной доски в другую
         source_list.delete(selected)
         if target_list:
             target_list.insert("end", task)
@@ -29,10 +28,22 @@ def create_task(event):
 # Создание главного окна
 root = ttk.Window(themename="flatly")
 
-FONT = ("monospace", 10)
+FONT = ("monospace", 10, "bold")
 
 root.title("Kanban Board")
 root.resizable(0, 0)
+
+
+# Подписи для колонок
+todo_label = ttk.Label(root, text="To Do", style="warning", font=FONT)
+in_progress_label = ttk.Label(root, text="In Progress", style='primary', font=FONT)
+done_label = ttk.Label(root, text="Done", style="info", font=FONT)
+
+# Расположение подписей
+todo_label.grid(row=0, column=0, pady=(20,0))
+in_progress_label.grid(row=0, column=1, pady=(20,0))
+done_label.grid(row=0, column=2, pady=(20,0))
+
 
 # Создание списков задач
 todo_list = tk.Listbox(root, height=10, width=30, font=FONT)
@@ -40,22 +51,21 @@ in_progress_list = tk.Listbox(root, height=10, width=30, font=FONT)
 done_list = tk.Listbox(root, height=10, width=30, font=FONT)
 
 
-
-# Расположение списков на доске
-todo_list.grid(row=0, column=0, padx=20, pady=20)
-in_progress_list.grid(row=0, column=1, padx=(0, 20), pady=20)
-done_list.grid(row=0, column=2, padx=(0, 20), pady=20)
+# Расположение списков
+todo_list.grid(row=1, column=0, padx=20, pady=20)
+in_progress_list.grid(row=1, column=1, padx=(0, 20), pady=20)
+done_list.grid(row=1, column=2, padx=(0, 20), pady=20)
 
 
 # Создание виджетов интерфейса
 add_label = ttk.Label(root, text="Add Task:", font=FONT)
-add_label.grid(row=1, column=0, pady=(0,20), padx=(0, 20), sticky='e')
+add_label.grid(row=2, column=0, pady=(0,20), padx=(0, 20), sticky='e')
 
 entry = ttk.Entry(root, width=30)
-entry.grid(row=1, column=1, pady=(0,20), padx=(0, 20), sticky="ew")
+entry.grid(row=2, column=1, pady=(0,20), padx=(0, 20), sticky="ew")
 
 add_button = ttk.Button(root, text="Add", width=10)
-add_button.grid(row=1, column=2, pady=(0,20), sticky='w')
+add_button.grid(row=2, column=2, pady=(0,20), sticky='w')
 
 
 # События

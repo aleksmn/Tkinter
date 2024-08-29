99578b67b7a453e47ac9bf2b9375614d7c7c1d1d
aleksmn <al-mn@yandex.ru>
2024-08-29 16:26:28 +0300

    fix

--- color-theme-maker/color_frame.py
+++ color-theme-maker/color_frame.py
@@ -9,96 +9,83 @@ class ColorFrame(ctk.CTkFrame):
 
         self.selected_color = '#' + self.red_value + self.green_value + self.blue_value
 
-        # Define Layout
-
-        self.color_box = ctk.CTkLabel(
-            self, fg_color='black', text='', height=100, width=100)
-        self.color_hex = ctk.CTkEntry(self, justify='center', width=100)
-        self.color_hex.insert('end', "#000000")
-
+        # Цветовой квадрат
+        self.color_box = ctk.CTkLabel(self, fg_color='black', text='', height=100, width=100)
         self.color_box.grid(row=0, column=0, columnspan=2, padx=35, pady=10)
+
+        # Вывод кода цвета
+        self.color_hex = ctk.CTkEntry(self, justify='center', width=100)
+        self.color_hex.insert('end', self.selected_color)
+
         self.color_hex.grid(row=1, column=0, columnspan=2)
 
-        # Input frame.
-
-        # RED
+        # Слайдеры
+        # слайдер для красного цвета
         self.red_label = ctk.CTkLabel(self, text="R")
-        self.red_slider = ctk.CTkSlider(self, from_=0, to=255, command=lambda value: self.get_value(value, 'red'))
+        self.red_slider = ctk.CTkSlider(self, from_=0, to=255, command=lambda value: self.get_value(value, "red"))
+        self.red_label.grid(row=2, column=0, padx=(5, 0))
+        self.red_slider.grid(row=2, column=1, padx=5)
         self.red_slider.set(0)
 
-        # GREEN
+        # слайдер для зеленого цвета
         self.green_label = ctk.CTkLabel(self, text="G")
-        self.green_slider = ctk.CTkSlider(self, from_=0, to=255,  command=lambda value: self.get_value(value, 'green'))
+        self.green_slider = ctk.CTkSlider(self, from_=0, to=255, command=lambda value: self.get_value(value, "green"))
+        self.green_label.grid(row=3, column=0, padx=(5, 0))
+        self.green_slider.grid(row=3, column=1, padx=5)
         self.green_slider.set(0)
 
-        # BLUE
+        # слайдер для синего цвета
         self.blue_label = ctk.CTkLabel(self, text="B")
-        self.blue_slider = ctk.CTkSlider(self, from_=0, to=255, command=lambda value: self.get_value(value, 'blue'))
+        self.blue_slider = ctk.CTkSlider(self, from_=0, to=255, command=lambda value: self.get_value(value, "blue"))
+        self.blue_label.grid(row=4, column=0, padx=(5, 0))
+        self.blue_slider.grid(row=4, column=1, padx=5)
         self.blue_slider.set(0)
 
-        # Размещаем все элементы:
-
-        self.red_label.grid(row=2, column=0, sticky='W', padx=(5, 0))
-        self.red_slider.grid(row=2, column=1, sticky='W', padx=5)
-
-        self.green_label.grid(row=3, column=0, sticky='W', padx=(5, 0))
-        self.green_slider.grid(row=3, column=1, sticky='W', padx=5)
-
-        self.blue_label.grid(row=4, column=0, sticky='W', padx=(5, 0))
-        self.blue_slider.grid(row=4, column=1, sticky='W', padx=5)
-
     def get_value(self, value, color):
-        """Get current value for red and update color box"""
 
-        # print(value, color)
+        # переведем в шестнадцатеричную систему
         value = hex(int(value))
-        value = value.lstrip('0x')
-        value = value.zfill(2)
+        value = value.strip("0x")
+        # дополняем нулями до двух символов 
+        value = value.rjust(2, "0")
 
-        if color == 'red':
+        if color == "red":
             self.red_value = value
-        if color == 'green':
+        if color == "green":
             self.green_value = value
-        if color == 'blue':
+        if color == "blue":
             self.blue_value = value
-        
-        # Вызов функции
+
         self.update_color()
 
-
-
     def update_color(self):
-        # print(red_value, green_value, blue_value)
-        self.selected_color = f'#{self.red_value}{self.green_value}{self.blue_value}'
-        # Имзеняем цвет квадрата
+        # Выбранный цвет
+        self.selected_color = '#' + self.red_value + self.green_value + self.blue_value
+        # Изменяем цвет квадрата
         self.color_box.configure(fg_color=self.selected_color)
         # Подставляем код цвета в поле для ввода
-        self.color_hex.delete(0, 'end')
+        self.color_hex.delete(0, "end")
         self.color_hex.insert(0, self.selected_color)
 
     def get_color(self):
+        # возвращаем выбранный цвет
         return self.selected_color
 
-
     def set_color(self, color):
+        r, g, b = color[1:3], color[3:5]
 
-        r, g, b = color[1:3], color[3:5], color[5:7]
-        self.red_value, self.green_value, self.blue_value = r, g, b
-
-        self.red_slider.set(int(r, 16))
-        self.green_slider.set(int(g, 16))
-        self.blue_slider.set(int(b, 16))
-
-        self.update_color()
+        print(color)
 
 
+
+   
 if __name__ == "__main__":
 
     app = ctk.CTk()
     app.title('Color')
     cf = ColorFrame(app)
     cf.grid(column=0, row=0)
-    #
-    cf.set_color('#ff00ff')
+
+    cf.set_color("#ff00ff")
     
     app.mainloop()

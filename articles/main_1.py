54918e6d6256f82f90ceb55b82ba84558945dfee
aleksmn <al-mn@yandex.ru>
2025-02-10 14:04:27 +0300

    init

---
+++ articles/main_1.py
@@ -0,0 +1,78 @@
+import ttkbootstrap as ttk
+from tkinter import messagebox, Listbox
+
+import file_connection
+
+# Содержимое статей (названия и текст статей)
+articles = file_connection.get_articles()
+
+# Функция для отображения выбранной статьи
+def show_article():
+    selected_index = listbox.curselection()
+    if selected_index:
+        title = listbox.get(selected_index)
+        article_text = articles[title]
+        
+        # создаем окно для чтения статьи
+        show_window = ttk.Toplevel(root)
+        show_window.configure(padx=20, pady=20)
+        show_window.resizable(0, 0)
+        show_window.title(title)
+
+        # Создание виджета Label для отображения названия статьи
+        title_label = ttk.Label(show_window, text=title, font=("Helvetica", 14))
+        title_label.grid(column=0, row=0)
+
+        # Создание текстового виджета для отображения текста статьи
+        textbox = ttk.Text(show_window, wrap='word')
+        textbox.grid(column=0, row=1)
+        textbox.insert('end', article_text)
+        textbox.configure(state="disabled")
+
+
+# Функция для добавления новой статьи
+def add_article():
+    global articles
+    add_window = ttk.Toplevel(root)
+    add_window.title("Добавить статью")
+    add_window.configure(padx=20, pady=20)
+
+    label_title = ttk.Label(add_window, text="Введите название статьи:")
+    label_title.grid()
+
+    entry_title = ttk.Entry(add_window)
+    entry_title.grid(sticky="we")
+
+# Создание окна
+root = ttk.Window(themename="superhero")
+root.title("Кошко-вики")
+root.resizable(0, 0)
+root.configure(padx=20, pady=20)
+# root.iconbitmap('icon.ico')
+
+# Создание списка статей
+listbox = Listbox(root)
+listbox.grid(column=0, row=0, columnspan=3, sticky="we")
+
+# Заполнение списка статьями
+for article in articles:
+    listbox.insert('end', article)
+
+# Создание кнопки "Прочитать"
+read_button = ttk.Button(root, text="Прочитать", style="info")
+read_button.grid(column=0, row=1)
+
+# Создание кнопки "Добавить статью"
+add_button = ttk.Button(root, text="Добавить статью")
+add_button.grid(column=1, row=1)
+
+# Удаление
+delete_button = ttk.Button(root, text="Удалить статью", style="danger")
+delete_button.grid(column=2, row=1)
+
+
+
+# Запуск приложения
+root.mainloop()
+
+
